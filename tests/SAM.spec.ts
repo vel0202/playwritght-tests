ce3e87140abbf67d1a99335a86bf2d523d426c5f
Vel <tgbelova75@gmail.com>
2026-06-22 09:39:07 +0500

    Initial commit: Playwright tests

---
+++ tests/SAM.spec.ts
@@ -0,0 +1,91 @@
+import { test, expect } from '@playwright/test';
+
+test('Проверка отображения элементов навигации', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Docs' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'MCP', exact: true })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'CLI', exact: true })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'API' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Node.js' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'GitHub repository' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Discord server' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Switch between dark and light' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Search (Control+k)' })).toBeVisible();
+});
+
+test('Проверка названия элементов навигации', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toContainText(
+    'Playwright',
+  );
+  await expect(page.getByRole('link', { name: 'Docs' })).toContainText('Docs');
+  await expect(page.getByRole('link', { name: 'MCP', exact: true })).toContainText('MCP');
+  await expect(page.getByRole('link', { name: 'CLI', exact: true })).toContainText('CLI');
+  await expect(page.getByRole('link', { name: 'API' })).toContainText('API');
+  await expect(page.getByRole('button', { name: 'Node.js' })).toContainText('Node.js');
+});
+
+test('Проверка атрибутов hrev элементов навигации', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page.getByRole('link', { name: 'Playwright logo Playwright' })).toHaveAttribute(
+    'href',
+    '/',
+  );
+  await expect(page.getByRole('link', { name: 'Docs' })).toHaveAttribute('href', '/docs/intro');
+  await expect(page.getByRole('link', { name: 'MCP', exact: true })).toHaveAttribute(
+    'href',
+    '/mcp/introduction',
+  );
+  await expect(page.getByRole('link', { name: 'CLI', exact: true })).toHaveAttribute(
+    'href',
+    '/agent-cli/introduction',
+  );
+  await expect(page.getByRole('link', { name: 'API' })).toHaveAttribute(
+    'href',
+    '/docs/api/class-playwright',
+  );
+  await expect(page.getByRole('link', { name: 'GitHub repository' })).toHaveAttribute(
+    'href',
+    'https://github.com/microsoft/playwright',
+  );
+  await expect(page.getByRole('link', { name: 'Discord server' })).toHaveAttribute(
+    'href',
+    'https://aka.ms/playwright/discord',
+  );
+});
+
+test('Проверка переключения темы', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('button', { name: 'Switch between dark and light' }).click();
+  await expect(page.locator('html')).toHaveAttribute('data-theme', 'light');
+  await page.getByRole('button', { name: 'Switch between dark and light' }).click();
+  await expect(page.locator('html')).toHaveAttribute('data-theme', 'dark');
+});
+
+test('Проверка заголовков', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page.getByRole('heading', { name: 'Playwright enables reliable' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Playwright enables reliable' })).toContainText(
+    'Playwright enables reliable web automation for testing, scripting, and AI agents.',
+  );
+  await expect(page.getByText('One API to drive Chromium,')).toBeVisible();
+  await expect(page.getByText('One API to drive Chromium,')).toContainText(
+    'One API to drive Chromium, Firefox, and WebKit — in your tests, your scripts, and your agent workflows. Available for TypeScript, Python, .NET, and Java.',
+  );
+});
+
+test('Побегаем по странице', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('button', { name: 'Switch between dark and light' }).click();
+  await expect(page.locator('html')).toHaveAttribute('data-theme', 'light');
+  await page.getByRole('button', { name: 'Switch between dark and light' }).click();
+  await expect(page.locator('html')).toHaveAttribute('data-theme', 'dark');
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toContainText('Installation');
+  await page.getByRole('link', { name: 'VS Code', exact: true }).click();
+  await expect(page.getByRole('heading', { name: 'VS Code' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'VS Code' })).toContainText('VS Code');
+  await page.getByRole('link', { name: 'Playwright logo Playwright' }).click();
+});
